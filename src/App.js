63f8d1d26f5a976b358ad443df654113bdb47b26
Tushar35515 <tk35515@gmail.com>
2023-09-20 02:43:38 +0530

    Basic design

--- src/App.js
+++ src/App.js
@@ -1,14 +1,96 @@
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       
-        <h1>
-          HELLO!!!
-        </h1>
+    <div className="trello-zuddl">
+      <nav className="navbar app"> App bar</nav>
+      <nav className="navbar board"> Board bar</nav>
         
-      </header>
+        <div className = "board-columns">
+          <div className = "column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img width="300" height="132" src="https://trello.com/1/cards/5e20e0e72365b93c7a291206/attachments/5e20e0e72365b93c7a291207/previews/5e20e0e72365b93c7a29120d/download/Design.png"/>
+               Design & Research
+            </li>
+            <li>second</li>
+            <li>third</li>
+            <li>second</li>
+            <li>third</li>
+            <li>second</li>
+            <li>third</li>
+            <li>second</li>
+            <li>third</li>
+            <li>second</li>
+            <li>third</li>
+            <li>second</li>
+            <li>third</li>
+          </ul>
+          <footer>Add another card</footer>
+          </div>
+
+          <div className = "column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img width="300" height="132"  src="https://trello.com/1/cards/5e20e0e72365b93c7a291206/attachments/5e20e0e72365b93c7a291207/previews/5e20e0e72365b93c7a29120d/download/Design.png"/>
+               Design & Research
+            </li>
+            <li>second</li>
+          </ul>
+          <footer>Add another card</footer>
+          </div>
+
+          <div className = "column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img width="300" height="132"  src="https://trello.com/1/cards/5e20e0e72365b93c7a291206/attachments/5e20e0e72365b93c7a291207/previews/5e20e0e72365b93c7a29120d/download/Design.png"/>
+               Design & Research
+            </li>
+            <li>second</li>
+          </ul>
+          <footer>Add another card</footer>
+          </div>
+
+          <div className = "column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img width="300" height="132"  src="https://trello.com/1/cards/5e20e0e72365b93c7a291206/attachments/5e20e0e72365b93c7a291207/previews/5e20e0e72365b93c7a29120d/download/Design.png"/>
+               Design & Research
+            </li>
+            <li>second</li>
+          </ul>
+          <footer>Add another card</footer>
+          </div>
+
+          <div className = "column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img width="300" height="132"  src="https://trello.com/1/cards/5e20e0e72365b93c7a291206/attachments/5e20e0e72365b93c7a291207/previews/5e20e0e72365b93c7a29120d/download/Design.png"/>
+               Design & Research
+            </li>
+            <li>second</li>
+          </ul>
+          <footer>Add another card</footer>
+          </div>
+
+          <div className = "column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img width="300" height="132"  src="https://trello.com/1/cards/5e20e0e72365b93c7a291206/attachments/5e20e0e72365b93c7a291207/previews/5e20e0e72365b93c7a29120d/download/Design.png"/>
+               Design & Research
+            </li>
+            <li>second</li>
+          </ul>
+          <footer>Add another card</footer>
+          </div>
+
+        </div>
+        
     </div>
   );
 }
