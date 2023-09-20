c3ad8195c513d1dd36bb0e37c042bbf3c601f94f
Tushar35515 <tk35515@gmail.com>
2023-09-20 10:56:33 +0530

    Adding Columns and Cards

--- src/App.js
+++ src/App.js
@@ -1,96 +1,13 @@
+import AppBar from "./components/AppBar/AppBar";
+import BoardBar from "./components/BoardBar/BoardBar";
+import BoardContent from "./components/BoardContent/BoardContent";
 
 function App() {
   return (
     <div className="trello-zuddl">
-      <nav className="navbar app"> App bar</nav>
-      <nav className="navbar board"> Board bar</nav>
-        
-        <div className = "board-columns">
-          <div className = "column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img width="300" height="132" src="https://trello.com/1/cards/5e20e0e72365b93c7a291206/attachments/5e20e0e72365b93c7a291207/previews/5e20e0e72365b93c7a29120d/download/Design.png"/>
-               Design & Research
-            </li>
-            <li>second</li>
-            <li>third</li>
-            <li>second</li>
-            <li>third</li>
-            <li>second</li>
-            <li>third</li>
-            <li>second</li>
-            <li>third</li>
-            <li>second</li>
-            <li>third</li>
-            <li>second</li>
-            <li>third</li>
-          </ul>
-          <footer>Add another card</footer>
-          </div>
-
-          <div className = "column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img width="300" height="132"  src="https://trello.com/1/cards/5e20e0e72365b93c7a291206/attachments/5e20e0e72365b93c7a291207/previews/5e20e0e72365b93c7a29120d/download/Design.png"/>
-               Design & Research
-            </li>
-            <li>second</li>
-          </ul>
-          <footer>Add another card</footer>
-          </div>
-
-          <div className = "column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img width="300" height="132"  src="https://trello.com/1/cards/5e20e0e72365b93c7a291206/attachments/5e20e0e72365b93c7a291207/previews/5e20e0e72365b93c7a29120d/download/Design.png"/>
-               Design & Research
-            </li>
-            <li>second</li>
-          </ul>
-          <footer>Add another card</footer>
-          </div>
-
-          <div className = "column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img width="300" height="132"  src="https://trello.com/1/cards/5e20e0e72365b93c7a291206/attachments/5e20e0e72365b93c7a291207/previews/5e20e0e72365b93c7a29120d/download/Design.png"/>
-               Design & Research
-            </li>
-            <li>second</li>
-          </ul>
-          <footer>Add another card</footer>
-          </div>
-
-          <div className = "column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img width="300" height="132"  src="https://trello.com/1/cards/5e20e0e72365b93c7a291206/attachments/5e20e0e72365b93c7a291207/previews/5e20e0e72365b93c7a29120d/download/Design.png"/>
-               Design & Research
-            </li>
-            <li>second</li>
-          </ul>
-          <footer>Add another card</footer>
-          </div>
-
-          <div className = "column">
-          <header>Brainstorm</header>
-          <ul>
-            <li>
-              <img width="300" height="132"  src="https://trello.com/1/cards/5e20e0e72365b93c7a291206/attachments/5e20e0e72365b93c7a291207/previews/5e20e0e72365b93c7a29120d/download/Design.png"/>
-               Design & Research
-            </li>
-            <li>second</li>
-          </ul>
-          <footer>Add another card</footer>
-          </div>
-
-        </div>
-        
+      <AppBar/>
+      <BoardBar/>
+      <BoardContent/>
     </div>
   );
 }
---
+++ src/components/Card/Card.js
@@ -0,0 +1,17 @@
+import './Card.scss'
+
+const Card = (props) => {
+    const {card} = props;
+    return(
+        <>
+            <li className = "card-item">
+                {
+                    card.image && <img className="card-cover" src={card.image}/>
+                }
+                {card.title}
+            </li>
+        </>
+    )
+}
+
+export default Card;
